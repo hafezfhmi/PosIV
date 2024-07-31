a08b7c7140813e88dec238b71c46fc3eb10e952c
hafezfhmi <hafezfahmi2000@gmail.com>
2024-07-31 12:57:50 +0800

    chore: add prettier to backend

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
@@ -9,6 +10,7 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
     ignores: ["dist/**", "node_modules/**"],
   },
